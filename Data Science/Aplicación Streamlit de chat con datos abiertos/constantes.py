1e89288739dae1b76ac4188871db7acdf44994ad
datos.gob.es <contacto@datos.gob.es>
2025-01-24 09:26:11 +0100

    Update constantes.py

--- Data Science/Aplicación Streamlit de chat con datos abiertos/constantes.py
+++ Data Science/Aplicación Streamlit de chat con datos abiertos/constantes.py
@@ -1,2 +1,3 @@
+#Notése que por simplicidad y porque la aplicación se ejecuta en un contexto individual y no productivo, se establecen las claves de API sin protección vía secrets o variables de entorno protegidas
 api_key = "TU_API_KEY"
 ngrok_token = "TU_TOKEN_NGROK"
