e3467dc57829f70b8da66cd2a978b63aa86de74d
JBsCorner <jbbtp00@gmail.com>
2025-12-04 00:36:04 +0100

    Delete Data Science/mi_carpeta directory

--- Data Science/mi_carpeta/unity_catalog/bdns_downloader/__init__.py
+++
@@ -1,8 +0,0 @@
-from .indice import descargar_indice_reanudable
-from .detalle import descargar_detalle_async, preparar_ids_detalle
-
-__all__ = [
-    "descargar_indice_reanudable",
-    "descargar_detalle_async",
-    "preparar_ids_detalle",
-]
